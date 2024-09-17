de99aa2741906d35df31a9a2b041e9e239da0b7f
aman8558 <105880487+aman8558@users.noreply.github.com>
2024-09-17 19:47:03 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
 import {BrowserRouter} from 'react-router-dom';
-import StoreContextProvider from './Context/StoreContext.jsx';
+import StoreContextProvider from './context/StoreContext.jsx';
 
 
 
